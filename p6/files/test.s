e7ff40bc460ae54e6b302ad250fa3a5d805f1695
trevor <trevorzb11@gmail.com>
2021-04-21 17:24:37 -0500

    p6 backup

---
+++ p6/files/test.s
@@ -0,0 +1,20 @@
+	.data
+	.align 4
+_a: .space 4
+	.data
+	.align 4
+_b: .space 4
+	.text
+	.globl main
+main:
+	sw    $ra, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	sw    $fp, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	addu  $fp, $sp, 8
+	subu  $sp, $sp, 8
+	lw    $ra, 0($fp)	#load return address
+	move  $t0, $fp		#save control link
+	lw    $fp, -4($fp)	#restore FP
+	move  $sp, $t0		#restore SP
+	jr    $ra		#return
